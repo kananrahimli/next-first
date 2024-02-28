45567820b5d522646c5561678bf745bfacf1b59b
vkananrahimli <rahimlikanan181@gmail.com>
2024-02-28 12:11:07 +0400

    last

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
+const page = async  ({params}:{params:{id:string}}) => {
+    const {id}=params
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+      ]);
+      if(!invoice){
+        notFound()
+      }
+      // ...
+  return (
+    <main>
+    <Breadcrumbs
+      breadcrumbs={[
+        { label: 'Invoices', href: '/dashboard/invoices' },
+        {
+          label: 'Edit Invoice',
+          href: `/dashboard/invoices/${id}/edit`,
+          active: true,
+        },
+      ]}
+    />
+    <Form invoice={invoice} customers={customers} id={id}/>
+  </main>
+
+  )
+}
+
+export default page
